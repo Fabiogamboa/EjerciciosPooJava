4538d1967f3b59681c4027c13d4583a79a387795
Fabiogamboa <150569632+Fabiogamboa@users.noreply.github.com>
2025-03-03 15:06:27 -0500

    Add files via upload

---
+++ ejercicio8/src/ejercicio8/Cliente.java
@@ -0,0 +1,22 @@
+package ejercicio8;
+
+import java.util.Random;
+
+public class Cliente {
+    public static Random random = new Random();
+    public static int membresia = random.nextInt(3) + 1;
+    public static int hora=random.nextInt(24)+1;
+    
+    public static void Validarentrada(){
+        if(Cliente.membresia==1 && Cliente.hora>=6 && Cliente.hora<=18){
+            String resultado="el cliente tiene membresia y pudo entrar";
+            Tienda.Tiendaentrada(resultado);
+        }else if (Cliente.membresia==2 && Cliente.hora>=6 && Cliente.hora<=18){
+            String resultado="el cliente es empleado y pudo entrar";
+            Tienda.Tiendaentrada(resultado);
+        }else{
+            String resultado="el cliente no pudo entrar";
+            Tienda.Tiendaentrada(resultado);
+        }
+    }
+}
---
+++ ejercicio8/src/ejercicio8/Tienda.java
@@ -0,0 +1,8 @@
+package ejercicio8;
+
+public class Tienda {
+    
+    public static void Tiendaentrada(String resultado){
+        System.out.println(resultado);
+    }
+}
